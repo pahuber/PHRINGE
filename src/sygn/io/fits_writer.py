5b74dccb15c30b534e18f712cb0853516d88ef36
pahuber <philhuber@live.com>
2024-02-07 19:15:39 +0100

    Add fits writer

--- src/sygn/io/fits_writer.py
+++ src/sygn/io/fits_writer.py
@@ -1,3 +1,19 @@
+from datetime import datetime
+from pathlib import Path
+
+from astropy.io import fits
+
+
 class FITSWriter():
-    def write(self, data):
-        print('data written')
+
+    def write(self, data, output_dir: Path = None):
+        primary = fits.PrimaryHDU()
+        header = primary.header
+        hdu_list = []
+        hdu_list.append(primary)
+        for data_per_output in data:
+            hdu = fits.ImageHDU(data_per_output)
+            hdu_list.append(hdu)
+        hdul = fits.HDUList(hdu_list)
+        output_dir = Path(output_dir) if output_dir else Path('.')
+        hdul.writeto(output_dir.joinpath(f'data_{datetime.now().strftime("%Y%m%d_%H%M%S.%f")}.fits'))
