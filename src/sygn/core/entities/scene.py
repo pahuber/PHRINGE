5805cef2e56735f7a3d11a1cb7af36b5c143ad82
pahuber <philhuber@live.com>
2024-02-07 19:16:17 +0100

    Add get all sources method

--- src/sygn/core/entities/scene.py
+++ src/sygn/core/entities/scene.py
@@ -18,13 +18,20 @@ class Scene(BaseComponent, BaseModel):
         super().__init__(**data)
         self.local_zodi = LocalZodi()
 
+    def get_all_sources(self):
+        """Return all sources in the scene that should be accounted for, i.e. only the ones for which a spectrum has
+        been generated/provided."""
+        sources = [*self.planets]
+        if self.star.mean_spectral_flux_density is not None:
+            sources.append(self.star)
+        if self.local_zodi.mean_spectral_flux_density is not None:
+            sources.append(self.local_zodi)
+        if self.exozodi.mean_spectral_flux_density is not None:
+            sources.append(self.exozodi)
+        return sources
+
     def prepare(self, settings, observatory, spectrum):
         """Prepare the system for the simulation."""
-        self.star.prepare(
-            settings.wavelength_steps,
-            settings.grid_size,
-            number_of_wavelength_steps=len(settings.wavelength_steps)
-        )
         for planet in self.planets:
             planet.prepare(
                 settings.wavelength_steps,
@@ -35,12 +42,21 @@ class Scene(BaseComponent, BaseModel):
                 star_mass=self.star.mass,
                 number_of_wavelength_steps=len(settings.wavelength_steps)
             )
-        self.local_zodi.prepare(
-            settings.wavelength_steps,
-            settings.grid_size,
-            field_of_view=observatory.field_of_view,
-            star_right_ascension=self.star.right_ascension,
-            star_declination=self.star.declination,
-            number_of_wavelength_steps=len(settings.wavelength_steps)
-        )
-        # self.exozodi.prepare(settings.time_steps, settings.wavelength_steps)
+        if settings.has_stellar_leakage:
+            self.star.prepare(
+                settings.wavelength_steps,
+                settings.grid_size,
+                number_of_wavelength_steps=len(settings.wavelength_steps)
+            )
+        if settings.has_local_zodi_leakage:
+            self.local_zodi.prepare(
+                settings.wavelength_steps,
+                settings.grid_size,
+                field_of_view=observatory.field_of_view,
+                star_right_ascension=self.star.right_ascension,
+                star_declination=self.star.declination,
+                number_of_wavelength_steps=len(settings.wavelength_steps)
+            )
+        if settings.has_exozodi_leakage:
+            pass
+            # self.exozodi.prepare(settings.time_steps, settings.wavelength_steps)
