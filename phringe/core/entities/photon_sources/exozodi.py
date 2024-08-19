Fix exozodi calculation

phringe/core/entities/photon_sources/exozodi.py
@@ -5,7 +5,6 @@ import torch
 from astropy.constants.codata2018 import h, c, k_B
 from astropy.units import Quantity
 from pydantic import BaseModel
-from tqdm import tqdm
 
 from phringe.core.entities.photon_sources.base_photon_source import BasePhotonSource
 from phringe.util.grid import get_radial_map, get_meshgrid
@@ -21,7 +20,7 @@ class Exozodi(BasePhotonSource, BaseModel):
     inclination: Any
     field_of_view_in_au_radial_maps: Any = None
 
-    def _calculate_spectral_flux_density(
+    def _get_spectral_flux_density(
             self,
             wavelength_steps: np.ndarray,
             grid_size: int,
@@ -31,53 +30,35 @@ class Exozodi(BasePhotonSource, BaseModel):
         star_distance = kwargs['star_distance']
         star_luminosity = kwargs['star_luminosity']
         field_of_view_in_au = field_of_view * star_distance * 6.68459e-12
+        num_wavelengths = len(field_of_view)
+        shape = (num_wavelengths, grid_size, grid_size)
 
-        temperature_map = torch.zeros((len(field_of_view_in_au), grid_size, grid_size), dtype=torch.float32)
-        self.field_of_view_in_au_radial_maps = torch.zeros(temperature_map.shape, dtype=torch.float32)
-        mean_spectral_flux_density = torch.zeros(temperature_map.shape, dtype=torch.float32)
+        self.field_of_view_in_au_radial_maps = torch.zeros(shape, dtype=torch.float32)
+        spectral_flux_density = torch.zeros(shape, dtype=torch.float32)
 
-        for index_fov, fov_in_au in enumerate(tqdm(field_of_view_in_au)):
+        for index_fov, fov_in_au in enumerate(field_of_view_in_au):
             self.field_of_view_in_au_radial_maps[index_fov] = get_radial_map(fov_in_au, grid_size)
 
-            temperature_map[index_fov] = self._calculate_temperature_profile(
-                self.field_of_view_in_au_radial_maps[index_fov],
-                star_luminosity
-            )
+        temperature_map = self._calculate_temperature_profile(
+            self.field_of_view_in_au_radial_maps,
+            star_luminosity
+        )
 
-            # Calculate the full spectrum for each wavelength (i.e. fov) only once for the first point in the grid_size
-            # x grid_size shaped temperature map and then calculate the spectra of all other (grid_size x grid_size) -1
-            # points through multiplying and dividing my the Planck law denominator. This is much faster than
-            # calculating the full spectrum for each point in the grid, since only one wavelength bin of the spectrum is
-            # used per point in the grid, so a lot of unnecessary calculations would be performed.
-            spectrum_at_0_0 = create_blackbody_spectrum(
-                temperature_map[index_fov][0][0],
-                wavelength_steps
-            ) * self.solid_angle[index_fov] ** 2
+        for ifov, fov in enumerate(field_of_view):
+            spectral_flux_density[ifov] = create_blackbody_spectrum(
+                temperature_map[ifov, :, :],
+                wavelength_steps[ifov, None, None]
+            ) * self.solid_angle[ifov, None, None]
 
-            mean_spectral_flux_density[index_fov][0][0] = spectrum_at_0_0[index_fov]
-            for index_x in range(1, grid_size):
-                for index_y in range(1, grid_size):
-                    mean_spectral_flux_density[index_fov][index_x][index_y] = \
-                        (spectrum_at_0_0[index_fov] *
-                         self._calculate_planck_law_denominator(
-                             temperature_map[index_fov][0][0],
-                             wavelength_steps[index_fov]
-                         ) /
-                         self._calculate_planck_law_denominator(
-                             temperature_map[index_fov][index_x][
-                                 index_y],
-                             wavelength_steps[index_fov]
-                         )
-                         )
-        return mean_spectral_flux_density
+        return spectral_flux_density
 
-    def _calculate_sky_brightness_distribution(self, grid_size: int, **kwargs) -> np.ndarray:
+    def _get_sky_brightness_distribution(self, grid_size: int, **kwargs) -> np.ndarray:
         star_luminosity = kwargs['star_luminosity']
         reference_radius_in_au = torch.sqrt(torch.tensor(star_luminosity))
         surface_maps = self.level * 7.12e-8 * (self.field_of_view_in_au_radial_maps / reference_radius_in_au) ** (-0.34)
         return surface_maps * self.spectral_flux_density
 
-    def _calculate_sky_coordinates(self, grid_size: int, **kwargs) -> Coordinates:
+    def _get_sky_coordinates(self, grid_size: int, **kwargs) -> Coordinates:
         field_of_view = kwargs['field_of_view']
         sky_coordinates = torch.zeros((2, len(field_of_view), grid_size, grid_size), dtype=torch.float32)
 
@@ -90,7 +71,7 @@ class Exozodi(BasePhotonSource, BaseModel):
                 (sky_coordinates_at_fov[0], sky_coordinates_at_fov[1]))
         return sky_coordinates
 
-    def _calculate_solid_angle(self, **kwargs) -> float:
+    def _get_solid_angle(self, **kwargs) -> float:
         """Calculate and return the solid angle of the exozodi.
 
         :param kwargs: Additional keyword arguments

phringe/core/entities/photon_sources/local_zodi.py
@@ -16,7 +16,7 @@ class LocalZodi(BasePhotonSource, BaseModel):
     """Class representation of a local zodi."""
     name: str = 'LocalZodi'
 
-    def _calculate_spectral_flux_density(
+    def _get_spectral_flux_density(
             self,
             wavelength_steps: np.ndarray,
             grid_size: int,
@@ -50,11 +50,11 @@ class LocalZodi(BasePhotonSource, BaseModel):
                             -0.4) * torch.cos(torch.tensor(ecliptic_latitude)) ** 2)) ** 0.5)
         return mean_spectral_flux_density
 
-    def _calculate_sky_brightness_distribution(self, grid_size: int, **kwargs) -> np.ndarray:
+    def _get_sky_brightness_distribution(self, grid_size: int, **kwargs) -> np.ndarray:
         grid = torch.ones((grid_size, grid_size), dtype=torch.float32)
         return torch.einsum('i, jk ->ijk', self.spectral_flux_density, grid)
 
-    def _calculate_sky_coordinates(self, grid_size, **kwargs) -> Coordinates:
+    def _get_sky_coordinates(self, grid_size, **kwargs) -> Coordinates:
         number_of_wavelength_steps = kwargs['number_of_wavelength_steps']
         field_of_view = kwargs['field_of_view']
 
@@ -66,7 +66,7 @@ class LocalZodi(BasePhotonSource, BaseModel):
                 (sky_coordinates_at_fov[0], sky_coordinates_at_fov[1]))
         return sky_coordinates
 
-    def _calculate_solid_angle(self, **kwargs) -> float:
+    def _get_solid_angle(self, **kwargs) -> float:
         """Calculate and return the solid angle of the local zodi.
 
         :param kwargs: Additional keyword arguments

phringe/core/entities/photon_sources/planet.py
@@ -130,7 +130,7 @@ class Planet(BasePhotonSource, BaseModel):
         """
         return validate_quantity_units(value=value, field_name=info.field_name, unit_equivalency=(u.deg,)).si.value
 
-    def _calculate_spectral_flux_density(self, wavelength_bin_centers: Tensor, grid_size: int, **kwargs) -> Tensor:
+    def _get_spectral_flux_density(self, wavelength_bin_centers: Tensor, grid_size: int, **kwargs) -> Tensor:
         """Calculate the spectral flux density of the planet in units of ph s-1 m-3. Use the previously generated
         reference spectrum in units of ph s-1 m-3 sr-1 and the solid angle to calculate it and bin it to the
         simulation wavelength bin centers.
@@ -169,7 +169,7 @@ class Planet(BasePhotonSource, BaseModel):
         #
         # return torch.asarray(binned_spectral_flux_density, dtype=torch.float32)
 
-    def _calculate_sky_brightness_distribution(self, grid_size: int, **kwargs) -> np.ndarray:
+    def _get_sky_brightness_distribution(self, grid_size: int, **kwargs) -> np.ndarray:
         """Calculate and return the sky brightness distribution.
 
         :param context: The context
@@ -208,7 +208,7 @@ class Planet(BasePhotonSource, BaseModel):
             sky_brightness_distribution[:, index_x, index_y] = self.spectral_flux_density
         return sky_brightness_distribution
 
-    def _calculate_sky_coordinates(self, grid_size, **kwargs) -> np.ndarray:
+    def _get_sky_coordinates(self, grid_size, **kwargs) -> np.ndarray:
         """Calculate and return the sky coordinates of the planet. Choose the maximum extent of the sky coordinates such
         that a circle with the radius of the planet's separation lies well (i.e. + 2x 20%) within the map. The construction
         of such a circle will be important to estimate the noise during signal extraction.
@@ -242,7 +242,7 @@ class Planet(BasePhotonSource, BaseModel):
             return self._get_coordinates(grid_size, time_steps[0], 0, has_planet_orbital_motion, star_distance,
                                          star_mass)
 
-    def _calculate_solid_angle(self, **kwargs) -> float:
+    def _get_solid_angle(self, **kwargs) -> float:
         """Calculate and return the solid angle of the planet.
 
         :param kwargs: The keyword arguments