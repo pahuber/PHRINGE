3b0ad5f7fe63c2d868c556ab1b3d799b12248f52
pahuber <philhuber@live.com>
2025-02-05 16:09:52 +0100

    Add option to pass dict directly

--- phringe/entities/configuration.py
+++ phringe/entities/configuration.py
@@ -9,9 +9,9 @@ class Configuration(BaseEntity):
     path: str = None
     config_dict: dict = None
 
-    def __init__(self, path):
+    def __init__(self, path: Path = None, config_dict: dict = {}):
         super().__init__()
-        self.config_dict = load_config(path)
+        self.config_dict = load_config(path) if path is not None else config_dict
 
     def load_config(path: Path):
         spec = importlib.util.spec_from_file_location("config", path)
