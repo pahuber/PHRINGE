90dac711e3a636970d4ea998eb0fa524699a8675
pahuber <philhuber@live.com>
2025-04-02 15:24:16 +0200

    Add template getter

--- phringe/main.py
+++ phringe/main.py
@@ -153,9 +153,10 @@ class PHRINGE:
         wavelength_bin_centers = wavelength_bin_centers[:, None, None, None]
         wavelength_bin_widths = wavelength_bin_widths[None, :, None, None, None]
         if flux.size == 1:
-            flux = flux[None, None, None, None, None]
+            flux = flux[None, None, None, None]
         else:
             flux = flux[None, :, None, None, None]
+
         x_position = np.array([x_position])[None, None, None, None]
         y_position = np.array([y_position])[None, None, None, None]
         amplitude = self._instrument._get_amplitude(self._device).cpu().numpy()
